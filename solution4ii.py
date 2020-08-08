39be5f2972e70efaf40793c6f67990af779e3363
Himnish <67916778+Himnish@users.noreply.github.com>
2020-08-08 14:42:37 +0000

    solution4ii.py

--- solution4ii.py
+++ solution4ii.py
@@ -4,7 +4,7 @@ def solution(entrances, exits, path):
     length_paths = len(path)
     length_exits = len(exits)
     bunn_count = 0
-    inter_paths = path[length_entrances:(length_paths-length_exits)      # To find all intermediate rooms
+    inter_paths = path[length_entrances:(length_paths-length_exits)]     # To find all intermediate rooms
     for i in range(length_paths - length_entrances - length_exits):      # Loop through range of length of intermediate rooms
         sum_range = sum(inter_paths[i])                                  # Sum of an intermediate room's possible number of bunnies allowed
         sum_enter = 0                                                    # Sum of bunnies that enter that room
